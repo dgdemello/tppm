8b9f1043e778a5c409d49e19f0b0294af42a4c2c
tiago.neves@uniriotec.br <tiago.neves@uniriotec.br@da1eba59-616f-b5b7-7334-76d30d86de74>
2013-07-24 15:02:21 +0000

    Criação dos pacotes básicos

--- trunk/tppm/src/tppm/views/TppmAboutBox.java
+++ trunk/tppm/src/tppm/views/TppmAboutBox.java
@@ -2,7 +2,7 @@
  * TppmAboutBox.java
  */
 
-package tppm;
+package tppm.views;
 
 import org.jdesktop.application.Action;
 
@@ -38,13 +38,13 @@ public class TppmAboutBox extends javax.swing.JDialog {
         javax.swing.JLabel imageLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(tppm.TppmApp.class).getContext().getResourceMap(TppmAboutBox.class);
+        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(tppm.views.TppmApp.class).getContext().getResourceMap(TppmAboutBox.class);
         setTitle(resourceMap.getString("title")); // NOI18N
         setModal(true);
         setName("aboutBox"); // NOI18N
         setResizable(false);
 
-        javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(tppm.TppmApp.class).getContext().getActionMap(TppmAboutBox.class, this);
+        javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(tppm.views.TppmApp.class).getContext().getActionMap(TppmAboutBox.class, this);
         closeButton.setAction(actionMap.get("closeAboutBox")); // NOI18N
         closeButton.setName("closeButton"); // NOI18N
 
