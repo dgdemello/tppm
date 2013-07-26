df8b21366f100a94688d36a0efb9e955760a569f
pedro.jardim@uniriotec.br <pedro.jardim@uniriotec.br@da1eba59-616f-b5b7-7334-76d30d86de74>
2013-07-26 03:05:16 +0000

    Melhorias na implementação da interface EmpregadoDAOXML e de seus testes.

--- trunk/tppm/test/tppm/dao/EmpregadoDAOTest.java
+++ trunk/tppm/test/tppm/dao/EmpregadoDAOTest.java
@@ -1,21 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package tppm.dao;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import static org.junit.Assert.*;
 import tppm.domains.Empregado;
 import tppm.exceptions.EmpregadoDAOException;
-import tppm.services.EmpregadoService;
-import static org.mockito.Mockito.*;
 
 /**
  *
@@ -23,15 +17,16 @@ import static org.mockito.Mockito.*;
  */
 public class EmpregadoDAOTest {
     
-    static final String CPF_VALIDO = "12862377775";
-    static final String NOME_VALIDO = "Tiago Neves";
-    static final String NOME_VALIDO_ALTERNATIVO = "Tiago de Araujo Neves";
-    static final String SEXO_VALIDO = "Masculino";
-    static final Date DATA_NASCIMENTO_VALIDA = new Date(1992 - 1900, 3, 14);
-    static final Date DATA_ADMISSAO_VALIDA = new Date(2011 - 1900, 2, 25);
-    static final Double SALARIO_VALIDO = 14.235;
-    static final Date DATA_DESLIGAMENTO_VALIDA = new Date(2012 - 1900, 10, 15);
+    final String CPF_VALIDO = "12862377775";
+    final String NOME_VALIDO = "Tiago Neves";
+    final String NOME_VALIDO_ALTERNATIVO = "Tiago de Araujo Neves";
+    final String SEXO_VALIDO = "Masculino";
+    final Date DATA_NASCIMENTO_VALIDA = new GregorianCalendar(1992, GregorianCalendar.APRIL, 14).getTime();
+    final Date DATA_ADMISSAO_VALIDA = new GregorianCalendar(2011, GregorianCalendar.APRIL, 14).getTime();
+    final Double SALARIO_VALIDO = 14235.00;
+    final Date DATA_DESLIGAMENTO_VALIDA = new GregorianCalendar(2012, GregorianCalendar.APRIL, 14).getTime();
     
+    EmpregadoDAOXML empregadoDAOXML;
     
     public EmpregadoDAOTest() {
     }
@@ -46,100 +41,65 @@ public class EmpregadoDAOTest {
     
     @Before
     public void setUp() {
-      Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
-      EmpregadoDAOXML empregadoDAOXML = new EmpregadoDAOXML();
-      
-      try{
+        Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
+        empregadoDAOXML = new EmpregadoDAOXML();
+        try{
             empregadoDAOXML.incluir(empregado);
-            
         } catch(EmpregadoDAOException e){
-            System.out.println("Erro ao incluir"+e.getMessage());
+            System.out.println("Erro ao incluir "+e.getMessage());
         } 
-
     }
     
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of procurar method, of class EmpregadoDAO.
-     */
     @Test
     public void testProcurarEmpregadoExistente() throws Exception {
-        EmpregadoDAOXML empregadoDAOXML = new EmpregadoDAOXML();
+        empregadoDAOXML = new EmpregadoDAOXML();
         Empregado empregadoEncontrado = new Empregado();
         Empregado empregadoExperado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
         empregadoEncontrado = empregadoDAOXML.procurar(CPF_VALIDO);
-        
+
         assertEquals(empregadoExperado, empregadoEncontrado);
         
     }
 
-    /**
-     * Test of incluir method, of class EmpregadoDAO.
-     */
     @Test
     public void testIncluir() throws Exception {
-      Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
-      EmpregadoDAOXML empregadoDAOXML = new EmpregadoDAOXML();
-      
-      try{
+        Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
+        empregadoDAOXML = new EmpregadoDAOXML();
+        
+        try{
             empregadoDAOXML.incluir(empregado);
-            
         } catch(EmpregadoDAOException e){
             System.out.println("Erro ao incluir "+e.getMessage());
         } 
 
     }
 
-    /**
-     * Test of excluir method, of class EmpregadoDAO.
-     */
     @Test
     public void testExcluir() throws Exception {
-        
-       EmpregadoDAOXML empregadoDAOXML = new EmpregadoDAOXML();
-       Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
-        
-       try{
+        empregadoDAOXML = new EmpregadoDAOXML();
+        Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA); 
+
+        try{
             empregadoDAOXML.excluir(empregado);
-            
         } catch(EmpregadoDAOException e){
             System.out.println("Erro ao excluir "+e.getMessage());
         } 
     }
 
-    /**
-     * Test of alterar method, of class EmpregadoDAO.
-     */
     @Test
     public void testAlterar() throws Exception {
-       EmpregadoDAOXML empregadoDAOXML = new EmpregadoDAOXML();
-       Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO_ALTERNATIVO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA);
-       
-       try{
+        empregadoDAOXML = new EmpregadoDAOXML();
+        Empregado empregado = new Empregado(CPF_VALIDO, NOME_VALIDO_ALTERNATIVO, SEXO_VALIDO, DATA_NASCIMENTO_VALIDA, DATA_ADMISSAO_VALIDA, SALARIO_VALIDO, DATA_DESLIGAMENTO_VALIDA);
+
+        try{
             empregadoDAOXML.alterar(empregado);
-            
         } catch(EmpregadoDAOException e){
             System.out.println("Erro ao alterar "+e.getMessage());
         } 
        
-    }
-
-    public class EmpregadoDAOImpl implements EmpregadoDAO {
-
-        public Empregado procurar(String cpf) throws EmpregadoDAOException {
-            return null;
-        }
-
-        public void incluir(Empregado empregado) throws EmpregadoDAOException {
-        }
-
-        public void excluir(Empregado empregado) throws EmpregadoDAOException {
-        }
-
-        public void alterar(Empregado empregado) throws EmpregadoDAOException {
-        }
     }
 }
