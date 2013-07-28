e5a6eb84db47c25a7d2a60cfab48605cf84f12cb
tiago.neves@uniriotec.br <tiago.neves@uniriotec.br@da1eba59-616f-b5b7-7334-76d30d86de74>
2013-07-28 04:09:44 +0000

    Pair Programming: Tiago + Pedro Implementação do RegraTaxaDeJurosDAO utilizando implementação em memória Adição das regras de taxa de juros no inicializador para o sistema carregar as taxas ao iniciar testes para a classe RegraTaxaDeJurosDAOMemoria criação do método equals na classe RegraTaxaDeJuros para comparar levando em conta todos os atributos

--- trunk/tppm/src/tppm/config/InicializadorDeDAOsMemoria.java
+++ trunk/tppm/src/tppm/config/InicializadorDeDAOsMemoria.java
@@ -5,9 +5,12 @@
 package tppm.config;
 
 import tppm.dao.RegraEmprestimoDAOMemoria;
+import tppm.dao.RegraTaxaDeJurosDAOMemoria;
 import tppm.domains.Empregado;
 import tppm.domains.RegraEmprestimo;
+import tppm.domains.RegraTaxaDeJuros;
 import tppm.exceptions.DAOExceptions.RegraEmprestimoDAOException;
+import tppm.exceptions.DAOExceptions.RegraTaxaDeJurosDAOException;
 import tppm.exceptions.ErroAoInicializarDAOsMemoriaException;
 
 /**
@@ -17,8 +20,9 @@ import tppm.exceptions.ErroAoInicializarDAOsMemoriaException;
 public class InicializadorDeDAOsMemoria {
 
     private RegraEmprestimoDAOMemoria regraEmprestimoDAOMemoria;
+    private RegraTaxaDeJurosDAOMemoria regraTaxaDeJurosDAOMemoria;
     
-    public static final RegraEmprestimo[] regras = {
+    public static final RegraEmprestimo[] regrasEmprestimo = {
         new RegraEmprestimo(Empregado.SEXO_MASCULINO, 21, 35, 5000.01, Empregado.SALARIO_MAXIMO, 0.7),
         new RegraEmprestimo(Empregado.SEXO_MASCULINO, 21, 35, Empregado.SALARIO_MINIMO, 5000.00, 0.6),
         new RegraEmprestimo(Empregado.SEXO_MASCULINO, 36, 45, 6000.01, Empregado.SALARIO_MAXIMO, 0.55),
@@ -29,20 +33,39 @@ public class InicializadorDeDAOsMemoria {
         new RegraEmprestimo(Empregado.SEXO_FEMININO, 45, 61, Empregado.SALARIO_MINIMO, Empregado.SALARIO_MAXIMO, 0.40)
     };
     
+    public static final RegraTaxaDeJuros[] regrasTaxaDeJuros = {
+        new RegraTaxaDeJuros(1, 3, 0.05),
+        new RegraTaxaDeJuros(4, 9, 0.065),
+        new RegraTaxaDeJuros(10, 12, 0.08),
+        new RegraTaxaDeJuros(13, 18, 0.1)
+    };
+    
     public InicializadorDeDAOsMemoria(){
         regraEmprestimoDAOMemoria = new RegraEmprestimoDAOMemoria();
+        regraTaxaDeJurosDAOMemoria = new RegraTaxaDeJurosDAOMemoria();
     }
     
     public void inicializar() throws ErroAoInicializarDAOsMemoriaException{
         inicializarRegrasDeEmprestimo();
+        inicializarRegrasDeTaxaDeJuros();
     }
     
     private void inicializarRegrasDeEmprestimo() throws ErroAoInicializarDAOsMemoriaException{
         try{
-            for(RegraEmprestimo regra : regras){
+            for(RegraEmprestimo regra : regrasEmprestimo){
                 regraEmprestimoDAOMemoria.incluir(regra);
             }
         } catch(RegraEmprestimoDAOException e){
+            throw new ErroAoInicializarDAOsMemoriaException(e.getMessage());
+        }
+    }
+    
+    private void inicializarRegrasDeTaxaDeJuros() throws ErroAoInicializarDAOsMemoriaException{
+        try{
+            for(RegraTaxaDeJuros regra : regrasTaxaDeJuros){
+                regraTaxaDeJurosDAOMemoria.incluir(regra);
+            }
+        } catch(RegraTaxaDeJurosDAOException e){
             throw new ErroAoInicializarDAOsMemoriaException(e.getMessage());
         }
     }
---
+++ trunk/tppm/test/tppm/dao/RegraTaxaDeJurosDAOMemoriaTest.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tppm.dao;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tppm.config.InicializadorDeDAOsMemoria;
+import tppm.config.TPPMConfig;
+import tppm.domains.RegraTaxaDeJuros;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tiago Neves + Pedro Jardim
+ */
+public class RegraTaxaDeJurosDAOMemoriaTest {
+    
+    static final int NUMERO_PRESTACOES_TESTE_PROCURAR = 2;
+    static final Double TAXA_TESTE_PROCURAR = 0.05;
+
+    static final int NUMERO_MINIMO_PRESTACOES = 1;
+    static final int NUMERO_MAXIMO_PRESTACOES = 18;
+    
+    static final int NUMERO_MINIMO_PRESTACOES_TESTE_INSERIR = 19;
+    static final int NUMERO_MAXIMO_PRESTACOES_TESTE_INSERIR = 25;
+    static final Double TAXA_TESTE_INSERIR = 0.15;
+    static final int NUMERO_PRESTACOES_TESTE_INSERIR = 22;
+    
+    static final int NUMERO_PRESTACOES_TESTE_EXCLUIR = 5;
+    
+    static final int NUMERO_PRESTACOES_TESTE_ALTERAR = 11;
+    static final Double TAXA_TESTE_ALTERAR = 0.9;
+    
+    static RegraTaxaDeJurosDAOMemoria regraTaxaDeJurosDAOMemoria;
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        regraTaxaDeJurosDAOMemoria = new RegraTaxaDeJurosDAOMemoria();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        regraTaxaDeJurosDAOMemoria.excluirTudo();
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+        for(RegraTaxaDeJuros regra : InicializadorDeDAOsMemoria.regrasTaxaDeJuros){
+            regraTaxaDeJurosDAOMemoria.incluir(regra);
+        }
+    }
+    
+    @After
+    public void tearDown() throws Exception  {
+        regraTaxaDeJurosDAOMemoria.excluirTudo();
+    }
+
+    @Test
+    public void testProcurar() throws Exception {
+        RegraTaxaDeJuros resultado = regraTaxaDeJurosDAOMemoria.procurar(NUMERO_PRESTACOES_TESTE_PROCURAR);
+        assertEquals(TAXA_TESTE_PROCURAR, resultado.getTaxaDeJuros(), TPPMConfig.DELTA_COMPARACAO_DOUBLE);
+    }
+
+    @Test
+    public void testObterNumeroMinimoPrestacoes() throws Exception {
+        int resultado = regraTaxaDeJurosDAOMemoria.obterNumeroMinimoPrestacoes();
+        assertEquals(NUMERO_MINIMO_PRESTACOES, resultado);
+    }
+
+    @Test
+    public void testObterNumeroMaximoPrestacoes() throws Exception {
+        int resultado = regraTaxaDeJurosDAOMemoria.obterNumeroMaximoPrestacoes();
+        assertEquals(NUMERO_MAXIMO_PRESTACOES, resultado);
+    }
+
+    @Test
+    public void testIncluir() throws Exception {
+        RegraTaxaDeJuros novaRegra = new RegraTaxaDeJuros(NUMERO_MINIMO_PRESTACOES_TESTE_INSERIR, NUMERO_MAXIMO_PRESTACOES_TESTE_INSERIR, TAXA_TESTE_INSERIR);
+        regraTaxaDeJurosDAOMemoria.incluir(novaRegra);
+        RegraTaxaDeJuros regraRetornada = regraTaxaDeJurosDAOMemoria.procurar(NUMERO_PRESTACOES_TESTE_INSERIR);
+        assertEquals(novaRegra, regraRetornada);
+    }
+
+    @Test
+    public void testExcluir() throws Exception {
+        RegraTaxaDeJuros regra = regraTaxaDeJurosDAOMemoria.procurar(NUMERO_PRESTACOES_TESTE_EXCLUIR);
+        regraTaxaDeJurosDAOMemoria.excluir(regra);
+        RegraTaxaDeJuros regraAposExcluir = regraTaxaDeJurosDAOMemoria.procurar(NUMERO_PRESTACOES_TESTE_EXCLUIR);
+        assertNull(regraAposExcluir);
+    }
+
+    @Test
+    public void testAlterar() throws Exception {
+        RegraTaxaDeJuros regra = regraTaxaDeJurosDAOMemoria.procurar(NUMERO_PRESTACOES_TESTE_ALTERAR);
+        regra.setTaxaDeJuros(TAXA_TESTE_ALTERAR);
+        regraTaxaDeJurosDAOMemoria.alterar(regra);
+        RegraTaxaDeJuros regraAposAlterar = regraTaxaDeJurosDAOMemoria.procurar(NUMERO_PRESTACOES_TESTE_ALTERAR);
+        assertEquals(TAXA_TESTE_ALTERAR, regraAposAlterar.getTaxaDeJuros());
+    }
+}
